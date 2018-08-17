7be3486ab298b2748ef0f0185c2bca60883092ed
huszad <huszi.09@gmail.com>
2018-08-17 17:29:26 +0200

    flights delay

--- R/flights.R
+++ R/flights.R
@@ -18,18 +18,15 @@ whichflight<-function(param="UA"){
   # http://localhost:5602/echo?param=DL
   # a ?param= ut?n ?rjuk a megfelel? cuccot
 
-  flights<-nycflights13::flights
+  flights<-data.table::as.data.table(nycflights13::flights)
 
 
   # a hi?nyz? adatokat kiszedem, mert nincs ?rtelme azokat ?tlagolni
   flights<-flights[rowSums(is.na(flights))==0,]
-  flights<-flights["carrier" == param]
-  x<-mean(flights[,"arr_delay"])
-  y<-mean(flights[,"dep_delay"])
-  z<-paste0(param, " carrier ", "mean_arr_delay"," ", x, " min",", ", "mean_dep_delay"," ", y, " min")
+  x<-flights[carrier == param,readr::.(mean_arr_delay=mean(arr_delay),mean_dep_delay=mean(dep_delay))]
+  y<-paste0(param, " carrier ", colnames(x)[1]," ", x[[1]], " min",", ", colnames(x)[2]," ", x[[2]], " min")
   #x<-flights[carrier == param,.(mean_arr_delay=mean(arr_delay),mean_dep_delay=mean(dep_delay)), by = flight]
   #x<-x[order(flight)]
-  return(z)
+  return(y)
   #flights[rowSums(is.na(flights))==0,]
 }
-
