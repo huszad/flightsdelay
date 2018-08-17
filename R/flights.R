a58a2ac8f9923ad76d195436a7274ed2790b5de2
huszad <huszi.09@gmail.com>
2018-08-17 17:34:49 +0200

    flights delay

--- R/flights.R
+++ R/flights.R
@@ -23,7 +23,7 @@ whichflight<-function(param="UA"){
 
   # a hi?nyz? adatokat kiszedem, mert nincs ?rtelme azokat ?tlagolni
   flights<-flights[rowSums(is.na(flights))==0,]
-  x<-flights[carrier == param, .(mean_arr_delay=mean(arr_delay),mean_dep_delay=mean(dep_delay))]
+  x<-flights[carrier == param, plyr::.(mean_arr_delay=mean(arr_delay),mean_dep_delay=mean(dep_delay))]
   y<-paste0(param, " carrier ", colnames(x)[1]," ", x[[1]], " min",", ", colnames(x)[2]," ", x[[2]], " min")
   #x<-flights[carrier == param,.(mean_arr_delay=mean(arr_delay),mean_dep_delay=mean(dep_delay)), by = flight]
   #x<-x[order(flight)]
