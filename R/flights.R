c4e68f806b7605fcbfd7d7c974357d00af8bb76e
huszad <huszi.09@gmail.com>
2018-08-17 17:09:19 +0200

    flights delay

--- R/flights.R
+++ R/flights.R
@@ -18,14 +18,14 @@ whichflight<-function(param="UA"){
   # http://localhost:5602/echo?param=DL
   # a ?param= ut?n ?rjuk a megfelel? cuccot
 
-  flights<-data.table::as.data.table(nycflights13::flights)
+  flights<-nycflights13::flights
 
 
   # a hi?nyz? adatokat kiszedem, mert nincs ?rtelme azokat ?tlagolni
   flights<-flights[rowSums(is.na(flights))==0,]
-  flights<-flights["carrier" == param]
-  x<-mean(flights[,"arr_delay"])
-  y<-mean(flights[,"dep_delay"])
+  flights<-flights[carrier == param]
+  x<-mean(flights[,arr_delay])
+  y<-mean(flights[,dep_delay])
   z<-paste0(param, " carrier ", "mean_arr_delay"," ", x, " min",", ", "mean_dep_delay"," ", y, " min")
   #x<-flights[carrier == param,.(mean_arr_delay=mean(arr_delay),mean_dep_delay=mean(dep_delay)), by = flight]
   #x<-x[order(flight)]
